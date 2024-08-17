b854f5537ac8078f36c2aa042aa1ac9bb821e694
Fernando de Oliveira <ffernandodeoliveira@gmail.com>
2024-08-17 13:15:57 -0300

    Cadastro de restaurante

--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -1,9 +1,11 @@
+import { useMutation } from '@tanstack/react-query'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
 import { Link, useNavigate } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
+import { registerRestaurant } from '@/api/register-restaurant'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
@@ -26,36 +28,42 @@ export function SignUp() {
     formState: { isSubmitting },
   } = useForm<SignUpForm>()
 
+  const { mutateAsync: registerRestaurantFn } = useMutation({
+    mutationFn: registerRestaurant,
+  })
+
   async function handleSignUp(data: SignUpForm) {
     try {
-      console.log(data)
-
-      await new Promise((resolve) => setTimeout(resolve, 200))
+      await registerRestaurantFn({
+        restaurantName: data.restaurantName,
+        managerName: data.managerName,
+        email: data.email,
+        phone: data.phone,
+      })
 
       toast.success('Restaurante cadastrado com sucesso!', {
         action: {
           label: 'Login',
-          onClick: () => {
-            navigate('/sign-in')
-          },
+          onClick: () => navigate(`/sign-in?email=${data.email}`),
         },
       })
-    } catch {
+    } catch (error) {
       toast.error('Erro ao cadastrar restaurante.')
     }
   }
 
   return (
     <>
-      <Helmet titleTemplate="Cadastro" />
+      <Helmet title="Cadastro" />
+
       <div className="p-8">
         <Button variant="ghost" asChild className="absolute right-8 top-8">
-          <Link to="/sign-in">Fazer Login</Link>
+          <Link to="/sign-in">Fazer login</Link>
         </Button>
 
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
-            <h1 className="fonte-semibold text-2xl tracking-tight">
+            <h1 className="text-2xl font-semibold tracking-tight">
               Criar conta grátis
             </h1>
             <p className="text-sm text-muted-foreground">
@@ -63,49 +71,48 @@ export function SignUp() {
             </p>
           </div>
 
-          <form onSubmit={handleSubmit(handleSignUp)} className="space-y-4">
+          <form className="space-y-4" onSubmit={handleSubmit(handleSignUp)}>
             <div className="space-y-2">
               <Label htmlFor="restaurantName">Nome do estabelecimento</Label>
               <Input
                 id="restaurantName"
                 type="text"
                 {...register('restaurantName')}
-              ></Input>
+              />
             </div>
 
             <div className="space-y-2">
-              <Label htmlFor="managerName">seu nome</Label>
+              <Label htmlFor="managerName">Seu nome</Label>
               <Input
                 id="managerName"
                 type="text"
                 {...register('managerName')}
-              ></Input>
+              />
             </div>
 
             <div className="space-y-2">
-              <Label htmlFor="email">seu e-mail</Label>
-              <Input id="email" type="email" {...register('email')}></Input>
+              <Label htmlFor="email">Seu e-mail</Label>
+              <Input id="email" type="email" {...register('email')} />
             </div>
 
             <div className="space-y-2">
-              <Label htmlFor="phone">seu celular</Label>
-              <Input id="phone" type="tel" {...register('phone')}></Input>
+              <Label htmlFor="phone">Seu celular</Label>
+              <Input id="phone" type="tel" {...register('phone')} />
             </div>
 
             <Button disabled={isSubmitting} className="w-full" type="submit">
-              Finalizar Cadastro
+              Finalizar cadastro
             </Button>
 
             <p className="px-6 text-center text-sm leading-relaxed text-muted-foreground">
               Ao continuar, você concorda com nossos{' '}
-              <a className="underline underline-offset-4" href="">
-                Termos de serviço
+              <a href="" className="underline underline-offset-4">
+                termos de serviço
               </a>{' '}
               e{' '}
-              <a className="underline underline-offset-4" href="">
+              <a href="" className="underline underline-offset-4">
                 políticas de privacidade
               </a>
-              .
             </p>
           </form>
         </div>
